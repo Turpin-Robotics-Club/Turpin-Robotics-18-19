56a2083eba2bfcbea94b3b979a16f5f1dfd56ff8
TJCrouse <44447767+TJCrouse@users.noreply.github.com>
2018-10-31 15:25:01 -0400

    Created an educational driving program

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driver_Control.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "mmK", group = "Tests")
+public class Driver_Control extends OpMode {
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+    @Override
+    public void init() {
+        frontLeft = hardwareMap.dcMotor.get("front_left");
+        frontRight = hardwareMap.dcMotor.get("front_right");
+        backLeft = hardwareMap.dcMotor.get("back_left");
+        backRight = hardwareMap.dcMotor.get("back_right");
+    }
+
+    public void loop(){
+        //                      Right                       Forwards            Turn
+        frontLeft.setPower(-gamepad1.left_stick_x - gamepad1.left_stick_y - gamepad1.right_stick_x);
+        frontRight.setPower(gamepad1.left_stick_x - gamepad1.left_stick_y + gamepad1.left_stick_x);
+        backLeft.setPower(gamepad1.left_stick_x - gamepad1.left_stick_y - gamepad1.right_stick_x);
+        backRight.setPower(-gamepad1.left_stick_x - gamepad1.left_stick_y + gamepad1.left_stick_x);
+    }
+
+}
